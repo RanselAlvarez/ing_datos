8a0b87828cfde11825fccc224aed8efdaa8e8046
RanselAlvarez <lesnaralvarez@gmail.com>
2026-06-03 16:50:47 -0400

    Mejorando ejercicio2

--- ejercicio2/main.py
+++ ejercicio2/main.py
@@ -6,13 +6,21 @@ from datetime import datetime
 url_base = "http://ipwho.is/"
 
 parametros = {
-            "ip": "",
-            "lang": "es"
+            "fields": "ip,country,city",
+            # "ip": "",
+            # "lang": "es"
 }
 
 
 respuesta = requests.get(url_base, params=parametros, timeout=5)
 
 respuesta_json = respuesta.json()
-print(respuesta_json)
+ip = respuesta_json.get("ip", "IP Desconocido")
+pais = respuesta_json.get("country", "Pais Desconocido")
+ciudad = respuesta_json.get("city", "Ciiudad Desconocida")
+
+print(f"Tu ip es: {ip}")
+print(f"Pais: {pais}")
+print(f"Ciudad: {ciudad}")
+
 
